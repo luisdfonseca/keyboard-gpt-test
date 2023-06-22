1788a7c36980c1eabe82c9ae3b236e76fc66c42f
Luis Daniel Fonseca <luis.fonseca@hey.com>
2023-06-21 17:43:33 -0700

    Initial Commit

---
+++ keyboard-gpt-test/keyboard_gpt_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyboard_gpt_testApp.swift
+//  keyboard-gpt-test
+//
+//  Created by Luis Fonseca on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct keyboard_gpt_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ keyboard-gpt-testUITests/keyboard_gpt_testUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  keyboard_gpt_testUITestsLaunchTests.swift
+//  keyboard-gpt-testUITests
+//
+//  Created by Luis Fonseca on 6/21/23.
+//
+
+import XCTest
+
+final class keyboard_gpt_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
